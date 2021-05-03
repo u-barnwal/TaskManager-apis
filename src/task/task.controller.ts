276abc5cf8f99eff895f175c7a2f64565916fe44
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-04 04:27:29 +0530

    add createOne controller for Task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateTaskDTO } from 'src/dto/create-task.dto';
+import { TaskService } from './task.service';
 
-@Controller('task')
-export class TaskController {}
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  public async createOne(@Body() createTaskRequest: CreateTaskDTO) {
+    const resp = await this.taskService.createOne(createTaskRequest);
+
+    return resp;
+  }
+}
