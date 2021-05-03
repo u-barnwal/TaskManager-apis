00fcd3bb67ec9eb15e63b3f9cf8b0b2a5b6f410b
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-04 03:54:19 +0530

    add database module and typeorm scripts

--- ormconfig.ts
+++ ormconfig.ts
@@ -13,7 +13,7 @@ const config: ConnectionOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'postgres',
+  username: 'root',
   password: '@123',
   database: 'task_manager',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
